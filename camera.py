4f669b26db54df555e7ca1bc97c9268d91b650c6
hkgsherlock <hkgsherlock@gmail.com>
2017-04-06 04:53:47 +0800

    imutils pivideostream

--- camera.py
+++ camera.py
@@ -1,11 +1,13 @@
 import cv2
+from imutils.video.pivideostream import PiVideoStream
 
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture(0)
+        self.video = PiVideoStream(resolution=self.resolution, framerate=self.framerate)
+        self.video.start()
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
