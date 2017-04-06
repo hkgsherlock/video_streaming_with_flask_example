aa3fe0ead9ea85869d9b29f9a7908cefbfad9d09
hkgsherlock <hkgsherlock@gmail.com>
2017-04-06 12:49:46 +0800

    diu

--- main.py
+++ main.py
@@ -4,75 +4,79 @@
 
 # RESTful!
 
-from Queue import Queue
-from threading import Thread
-
 import cv2
-import numpy as np
 from flask import Flask, Response
 from flask.ext.classy import FlaskView, route
 
 from imutils.video.pivideostream import PiVideoStream
 
 
-class StreamingAndWebApi:
+class StreamingAndWebApi(object):
     def __init__(self):
-        self.streamingBuffer = self.StreamingBuffer()
-
         self.app = Flask(__name__)
-        self.WebApiView(self.streamingBuffer).register(self.app, route_base='/', subdomain='api')
+        self.WebApiView.register(self.app, route_base='/', subdomain='api')
         self.app.run(host='0.0.0.0', port=5000, debug=True)
 
     class WebApiView(FlaskView):
-        def __init__(self, streamingBuffer):
+        def __init__(self):
             super(StreamingAndWebApi.WebApiView, self).__init__()  # this is python 2 so ...
-            self.streamingBuffer = streamingBuffer
+            self.streamingBuffer = StreamingBuffer.getInstance()
 
         @route('/video_feed')
         def video_feed(self):
             return Response(self.streamingBuffer.gen(),
                             mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    class StreamingBuffer:
-        def __init__(self):
-            self.video = PiVideoStream(resolution=(854, 480), framerate=30)
-            self.video.start()
 
-            # self.pushing = True
-            # t = Thread(target=self.push(), args=())
-            # t.daemon = True
-            # t.start()
+class StreamingBuffer:
+    _INSTANCE = None
 
-            # self.frame_queue = Queue()
-            # self.last_frame = self.encode(np.zeros((854, 480, 3), np.uint8))
+    @staticmethod
+    def getInstance():
+        if StreamingBuffer._INSTANCE is None:
+            StreamingBuffer._INSTANCE = StreamingBuffer()
+        return StreamingBuffer._INSTANCE
 
-        def __del__(self):
-            self.pushing = False
+    def __init__(self):
+        self.video = PiVideoStream(resolution=(854, 480), framerate=30)
+        self.video.start()
 
-        @staticmethod
-        def encode(frame):
-            return cv2.imencode('.jpg', frame)
+        # self.pushing = True
+        # t = Thread(target=self.push(), args=())
+        # t.daemon = True
+        # t.start()
 
-        # def push(self):
-        #     while self.pushing:
-        #         if self.frame_queue.qsize() > 128:
-        #             self.frame_queue.get_nowait()
-        #         frame = self.encode(self.frame_queue.get())
-        #         self.frame_queue.put(frame)
-        #         self.last_frame = frame
+        # self.frame_queue = Queue()
+        # self.last_frame = self.encode(np.zeros((854, 480, 3), np.uint8))
 
-        def gen(self):
-            while True:
-                # if self.frame_queue.empty():
-                #     frame = self.last_frame
-                # else:
-                #     frame = self.frame_queue.get()
-                frame = self.encode(self.video.read())
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n\r\n')
+    def __del__(self):
+        self.pushing = False
 
-        # def putNewFrame(self, cv2Frame):
-        #     self.frame_queue.put(cv2Frame)
+    @staticmethod
+    def encode(frame):
+        return cv2.imencode('.jpg', frame)
+
+    # def push(self):
+    #     while self.pushing:
+    #         if self.frame_queue.qsize() > 128:
+    #             self.frame_queue.get_nowait()
+    #         frame = self.encode(self.frame_queue.get())
+    #         self.frame_queue.put(frame)
+    #         self.last_frame = frame
+
+    def gen(self):
+        while True:
+            # if self.frame_queue.empty():
+            #     frame = self.last_frame
+            # else:
+            #     frame = self.frame_queue.get()
+            frame = self.encode(self.video.read())
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n\r\n')
+
+            # def putNewFrame(self, cv2Frame):
+            #     self.frame_queue.put(cv2Frame)
+
 
 if __name__ == '__main__':
     StreamingAndWebApi()
